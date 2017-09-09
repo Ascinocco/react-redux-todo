524a883f35f9c969e0025e4318aa1365fdd77c96
Anthony Scinocco <scinocco.a@gmail.com>
2017-09-08 20:05:58 -0400

    scaffold basic react and bootstrap template

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './bootstrap.min.css';
+
+import TodoList from '../TodoList';
+import SearchBar from '../SearchBar';
+import TodoListSideBar from '../TodoListsSideBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <SearchBar/>
+        <div className="container">
+          <div className="row">
+            <TodoListSideBar/>
+            <TodoList/>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
